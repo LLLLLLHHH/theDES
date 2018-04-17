34bffb6e3498b24524e88e5e2903a76935e3e9f7
LLLLLLHHH <736061125@qq.com>
2018-04-17 19:56:21 +0800

    2。1

--- deswork/main.c
+++ deswork/main.c
@@ -303,7 +303,7 @@ void printfdiff(diffop* op) {
 		for (j = 0;j < 4;j++) {
 			printf("%d", temp[j]);
 		}printf("   	->  	");
-		while (pointer -> next != NULL) {
+		while (pointer != NULL) {
 			if (pointer->num != -1) {
 				IntToBit(pointer->lie, temp, 4);IntToBit(pointer->hang, hang, 2);
 				printf("%d%d%d%d%d%d   ", hang[0], temp[0], temp[1], temp[2], temp[3], hang[1]);
@@ -325,34 +325,39 @@ void differcal(int cha,int sbox) {
 	int hang,lie;
 	diffop* pointer=NULL;
 	
-	int i,j;
+	int i,j,x;int temp;bool data[6],data2[6];
 	diffop s_op[80];intts_op(s_op);//s_op=(diffop*)malloc(sizeof(diffop) * 80);
 	bool used[64] = { 0 };
 	for (i = 0;i < 64;i++) {
 		if (used[i] == 0) {
-			int temp;
-			
+			if (i == 6) {
+				i = 6;
+			};
 			used[i] = 1;
-			bool data[6];
 			IntToBit(i, data, 6);
 			hang = data[0] * 2 + data[5];
 			lie = data[4] + data[3] * 2 + data[2] * 4 + data[1] * 8;
 			s_op[16 + i].hang = hang;
 			s_op[16 + i].lie = lie;
-			temp = S_Box[sbox][hang][lie];
-			s_op[16 + i].num = temp;
+			s_op[16 + i].num = S_Box[sbox][hang][lie];
 
 			Xor(data, chafen, 6);
+
 			hang = data[0] * 2 + data[5];
 			lie = data[4] + data[3] * 2 + data[2] * 4 + data[1] * 8;
 			j = hang * 16 + lie;
-			used[j] = 1;
-			s_op[16 + j].hang = hang;
-			s_op[16 + j].lie = lie;
-			s_op[16 + j].num = S_Box[sbox][hang][lie];
-			s_op[16 + i].next = &s_op[16 + j];
+			x = data[5] + data[4] * 2 + data[3] * 4 + data[2] * 8 + data[1] * 16 + data[0] * 32;
+			used[x] = 1;
+			s_op[16 + x].hang = hang;
+			s_op[16 + x].lie = lie;
+			s_op[16 + x].num = S_Box[sbox][hang][lie];
+			s_op[16 + i].next = &s_op[16 + x];
 
-			temp = abs(temp - S_Box[sbox][hang][lie]);
+			//temp = abs(s_op[16 + i].num - s_op[16 + x].num);//计算输出差分
+			IntToBit(s_op[16 + i].num, data, 4);
+			IntToBit(s_op[16 + x].num, data2, 4);
+			Xor(data, data2, 4);
+			temp = data[3] + data[2] * 2 + data[1] * 4 + data[0] * 8;
 			pointer = &s_op[temp];
 
 			while (pointer->next != NULL) {
@@ -377,10 +382,10 @@ int main() {
 	printf("输入差分值(0-63) ：");
 	scanf("%d", &cha);
 	
-	printf("输入s盒值 ：");
+	printf("输入s盒值 ：(1-8)");
 	scanf("%d", &sbno);
 
-	differcal(cha, sbno);
+	differcal(cha, sbno-1);
 
 	getchar();
 	getchar();
